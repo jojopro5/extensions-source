6917801e23ae6a830869604ab46d6ae19c016bdd
jojopro5 <65901798+jojopro5@users.noreply.github.com>
2026-06-07 13:34:43 +1000

    Update Harimanga base URL

--- src/en/harimanga/src/eu/kanade/tachiyomi/extension/en/harimanga/Harimanga.kt
+++ src/en/harimanga/src/eu/kanade/tachiyomi/extension/en/harimanga/Harimanga.kt
@@ -6,7 +6,7 @@ import eu.kanade.tachiyomi.network.interceptor.rateLimit
 class Harimanga :
     Madara(
         "Harimanga",
-        "https://harimanga.me",
+        "https://harimanga.vip",
         "en",
     ) {
     override val client = super.client.newBuilder()
